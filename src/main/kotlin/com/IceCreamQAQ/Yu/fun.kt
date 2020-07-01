64382aa681958c6bdbd7a624a42e2b47beb50ad2
IceCream <www@withdata.net>
2020-07-01 18:07:58 +0800

    添加规范的参数验证错误异常

--- src/main/kotlin/com/IceCreamQAQ/Yu/fun.kt
+++ src/main/kotlin/com/IceCreamQAQ/Yu/fun.kt
@@ -1,6 +1,7 @@
 package com.IceCreamQAQ.Yu
 
 import com.IceCreamQAQ.Yu.annotation.NotSearch
+import com.IceCreamQAQ.Yu.util.YuParaValueException
 import com.alibaba.fastjson.JSON
 import java.lang.reflect.Modifier
 
@@ -8,3 +9,5 @@ fun Class<*>.isBean()= !(this.isInterface || Modifier.isAbstract(this.modifiers)
 
 fun <T> String.toObject(clazz: Class<T>) = JSON.parseObject(this,clazz)
 fun Any.toJSONString() = JSON.toJSONString(this)
+
+fun paraError(text:String) = YuParaValueException(text)
---
+++ src/main/kotlin/com/IceCreamQAQ/Yu/util/error.kt
@@ -0,0 +1,5 @@
+package com.IceCreamQAQ.Yu.util
+
+import java.lang.RuntimeException
+
+class YuParaValueException(message: String):RuntimeException(message)
