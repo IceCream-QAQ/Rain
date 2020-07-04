1e3771950f66ecf0f6fba4144e37a7ca0d997378
IceCream <www@withdata.net>
2020-07-05 00:11:25 +0800

    完成 Github #5

--- src/main/kotlin/com/IceCreamQAQ/Yu/job/JobManager.kt
+++ src/main/kotlin/com/IceCreamQAQ/Yu/job/JobManager.kt
@@ -37,6 +37,24 @@ class JobManager : ApplicationService {
         return registerTimer(runnable, firstTime, nextTime)
     }
 
+    @JvmOverloads
+    fun registerTimer(runnable: Runnable, atTime: String, always: Boolean = true): String {
+        val id = UUID.randomUUID().toString()
+        val syncTimer = Timer()
+        registerTimer[id] = (syncTimer)
+        val t = getTime(atTime)
+        if (always) {
+            val nt = (if (atTime.contains(":")) 24 * 60 * 60 * 1000 else 60 * 60 * 1000).toLong()
+            syncTimer.schedule(RegisterTask(id, runnable, this, true), t, nt)
+        } else syncTimer.schedule(RegisterTask(id, runnable, this, false), t)
+        return id
+    }
+
+    fun registerTimer(atTime: String, always: Boolean = true, function: () -> Unit): String {
+        val runnable = Runnable { function() }
+        return registerTimer(runnable, atTime, always)
+    }
+
     fun deleteTimer(id: String) {
         registerTimer[id]?.cancel()
         registerTimer.remove(id)
@@ -59,25 +77,7 @@ class JobManager : ApplicationService {
         for (job in jobs) {
             val ft =
                     if (job.at == "") job.time
-                    else {
-                        val t = job.at
-                        val d = t.contains(":")
-                        val date = Date()
-
-                        if (d) {
-                            val ds = dateUtil.formatDate(date)
-                            val dd = dateUtil.parseDateTime("$ds $t:00")
-                            var tt = dd.time - date.time
-                            if (tt < 0) tt += 24 * 60 * 60 * 1000
-                            tt
-                        } else {
-                            val ds = dateUtil.formatDateTime(date)
-                            val dd = dateUtil.parseDateTime("${ds.subSequence(0, 14)}$t:00")
-                            var tt = dd.time - date.time
-                            if (tt < 0) tt += 60 * 60 * 1000
-                            tt
-                        }
-                    }
+                    else getTime(job.at)
 
             if (job.async) asyncTimer?.schedule(job, ft, job.time)
             else {
@@ -85,6 +85,25 @@ class JobManager : ApplicationService {
                 syncTimers.add(syncTimer)
                 syncTimer.schedule(job, ft, job.time)
             }
+        }
+    }
+
+    fun getTime(time: String): Long {
+        val d = time.contains(":")
+        val date = Date()
+
+        return if (d) {
+            val ds = dateUtil.formatDate(date)
+            val dd = dateUtil.parseDateTime("$ds $time:00")
+            var tt = dd.time - date.time
+            if (tt < 0) tt += 24 * 60 * 60 * 1000
+            tt
+        } else {
+            val ds = dateUtil.formatDateTime(date)
+            val dd = dateUtil.parseDateTime("${ds.subSequence(0, 14)}$time:00")
+            var tt = dd.time - date.time
+            if (tt < 0) tt += 60 * 60 * 1000
+            tt
         }
     }
 
