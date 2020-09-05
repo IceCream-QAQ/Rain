19139d177eb0914c4633c500c45f7d2270c1ce8e
IceCream <www@withdata.net>
2020-09-06 00:06:36 +0800

    移除古老的 Controller 实现

--- src/main/kotlin/com/IceCreamQAQ/Yu/controller/router/MethodInvoker.kt
+++
@@ -1,9 +0,0 @@
-package com.IceCreamQAQ.Yu.controller.router
-
-import com.IceCreamQAQ.Yu.controller.ActionContext
-
-@Deprecated("已经弃用")
-interface MethodInvoker {
-    @Throws(Exception::class)
-    fun invoke(context: ActionContext): Any?
-}
