b77b51145e457c6bf388f928c047b2063db65434
IceCream <www@withdata.net>
2020-11-15 13:28:46 +0800

    新增 InvokerCompiler

--- src/main/kotlin/com/IceCreamQAQ/Yu/error/ers.kt
+++ src/main/kotlin/com/IceCreamQAQ/Yu/error/ers.kt
@@ -1,4 +1,8 @@
 package com.IceCreamQAQ.Yu.error
 
+import org.eclipse.jdt.core.compiler.CategorizedProblem
+
 class BeanCreateError(message: String) : Exception(message)
 class ControllerLoadErr(message: String) : Exception(message)
+
+class InvokerClassCreateException(message: String, pb: Array<CategorizedProblem>, cause: Throwable? = null) : RuntimeException(message, cause)
