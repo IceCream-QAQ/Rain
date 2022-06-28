93b15493bdfd85cb8dc2353718c561a656be7bdf
dengyidian <dengyidian.hyosakura@bytedance.com>
2022-06-28 14:28:45 +0800

    :recycle: refactor(architecture): refactor some unreasonable code

--- src/main/kotlin/com/IceCreamQAQ/Yu/di/YuContext.kt
+++ src/main/kotlin/com/IceCreamQAQ/Yu/di/YuContext.kt
@@ -430,7 +430,7 @@ class ValueObj<T>(var obj: T) : ReadWriteProperty<Any, T> {
     }
 }
 
-class MultiModeNotSupport<T> : ReadWriteProperty<Any, T> {
+class PrototypeNotSupport<T> : ReadWriteProperty<Any, T> {
     override fun getValue(thisRef: Any, property: KProperty<*>): T {
         error("当 YuContext 不处于 singleton 模式时，不允许 Kotlin inject 方式注入！")
     }
@@ -442,13 +442,13 @@ class MultiModeNotSupport<T> : ReadWriteProperty<Any, T> {
 }
 
 inline fun <reified T> inject(name: String = ""): ReadWriteProperty<Any, T> =
-    if (context == null) MultiModeNotSupport()
+    if (context == null) PrototypeNotSupport()
     else ValueObj(context!!.getBean(T::class.java, name)!!)
 
 inline fun <reified T> config(name: String): ReadWriteProperty<Any, T> =
-    if (context == null) MultiModeNotSupport()
+    if (context == null) PrototypeNotSupport()
     else ValueObj(context!!.manager.get(name, T::class.java)!!)
 
 inline fun <reified T> configArray(name: String): ReadWriteProperty<Any, List<T>> =
-    if (context == null) MultiModeNotSupport()
+    if (context == null) PrototypeNotSupport()
     else ValueObj(context!!.manager.getArray(name, T::class.java)!!)
