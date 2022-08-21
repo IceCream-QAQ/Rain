c7bfc1602eff87e437e9d1e21fccfadee0cedc87
IceCream <www@withdata.net>
2022-08-22 00:26:45 +0800

    为 Config 注解中的 value 字段提供默认值。

--- src/main/java/com/IceCreamQAQ/Yu/annotation/Config.java
+++ src/main/java/com/IceCreamQAQ/Yu/annotation/Config.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Config {
-    String value();
+    String value() default "";
 }
