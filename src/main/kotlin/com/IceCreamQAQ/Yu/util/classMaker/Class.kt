01841b4d2c3d6d2f6f8bccafe1b1234d944ce4c0
IceCream <www@withdata.net>
2022-10-01 06:11:14 +0800

    添加通用 API MClass。#67

---
+++ src/main/kotlin/com/IceCreamQAQ/Yu/util/classMaker/Class.kt
@@ -0,0 +1,33 @@
+package com.IceCreamQAQ.Yu.util.classMaker
+
+abstract class MClass<T>(
+    val name: String,
+    val superClass: Class<T>
+) : AccessAble, AbstractAble, StaticAble, FinalAble, AnnotationAble {
+
+    override var access: Access = Access.PUBLIC
+    override var abstract: Boolean = false
+    override var static: Boolean = false
+    override var final: Boolean = false
+
+    val interfaceClass: MutableList<Class<*>> = ArrayList()
+
+    override val annotations: MutableList<MAnnotation<*>> = ArrayList()
+
+    val initBlocks: MutableList<MInitBlock> = ArrayList()
+    val staticBlocks: MutableList<MStaticBlock> = ArrayList()
+
+    val constructors: MutableList<MConstructor> = ArrayList()
+    val fields: MutableList<MField<*>> = ArrayList()
+    val methods: MutableList<MMethod<*>> = ArrayList()
+
+    abstract fun make(): Class<Any>
+}
+
+abstract class MInitBlock
+abstract class MStaticBlock
+
+abstract class MConstructor
+
+
+
