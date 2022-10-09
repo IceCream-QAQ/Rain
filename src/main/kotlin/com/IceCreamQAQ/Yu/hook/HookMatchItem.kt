d97b243d4bfc8b5781962ca3b7478974acc7b5e7
IceCream <www@withdata.net>
2022-10-09 11:32:19 +0800

    添加 NoMatchHookItem

--- src/main/kotlin/com/IceCreamQAQ/Yu/hook/HookMatchItem.kt
+++ src/main/kotlin/com/IceCreamQAQ/Yu/hook/HookMatchItem.kt
@@ -3,3 +3,16 @@ package com.IceCreamQAQ.Yu.hook
 interface HookMatchItem {
     operator fun invoke(clazz: String, method: String, descriptor: String): Boolean
 }
+
+class NoMatchHookItem @JvmOverloads constructor(
+    val clazz: String,
+    val method: String,
+    val descriptor: String? = null
+) : HookMatchItem {
+    override fun invoke(clazz: String, method: String, descriptor: String): Boolean {
+        if (clazz != this.clazz) return false
+        if (method != this.method) return false
+        this.descriptor?.let { if (descriptor != it) return false }
+        return true
+    }
+}
