d2fe4461a30754a1aa4cea44dd572c4f6d812259
IceCream <www@withdata.net>
2022-10-09 11:26:51 +0800

    HookRunnableInfo 添加 descriptor

--- src/main/kotlin/com/IceCreamQAQ/Yu/hook/HookRunnableInfo.kt
+++ src/main/kotlin/com/IceCreamQAQ/Yu/hook/HookRunnableInfo.kt
@@ -2,6 +2,7 @@ package com.IceCreamQAQ.Yu.hook
 
 data class HookRunnableInfo(
     val name: String,
+    val descriptor: String,
     val instanceMode: Boolean,
     val clazz: Class<out HookRunnable>
 )
