13e9515cb3728a17ad9a0d06b4a3e04a14c83440
IceCream <www@withdata.net>
2022-10-11 22:48:31 +0800

    对 IRainClassloader 提供 hook，使其对 Classloader 唯一。

--- src/main/kotlin/com/IceCreamQAQ/Yu/loader/IRainClassLoader.kt
+++ src/main/kotlin/com/IceCreamQAQ/Yu/loader/IRainClassLoader.kt
@@ -1,6 +1,11 @@
 package com.IceCreamQAQ.Yu.loader
 
+import com.IceCreamQAQ.Yu.loader.transformer.ClassTransformer
+
 interface IRainClassLoader {
+
+    var hook: ClassTransformer
+
     fun define(name: String, data: ByteArray): Class<*>
 
     fun forName(name: String, initialize: Boolean): Class<*>
