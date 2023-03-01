61cbb792a02d4a67eceb4bc537751336e987dfaf
IceCream <www@withdata.net>
2023-03-02 03:26:07 +0800

    调整结构

--- src/main/kotlin/com/IceCreamQAQ/Yu/controller/RootRouterProcessFlowInfo.kt
+++ src/main/kotlin/com/IceCreamQAQ/Yu/controller/RootRouterProcessFlowInfo.kt
@@ -1,8 +1,8 @@
 package com.IceCreamQAQ.Yu.controller
 
 open class RootRouterProcessFlowInfo<T : ActionContext, R : Router>(
-    open val controllers: MutableList<ControllerProcessFlowInfo<T>> = arrayListOf(),
-    open val channels: Map<String, R> = hashMapOf(),
+    open val controllers: MutableList<ControllerProcessFlowInfo<T,R>> = arrayListOf(),
+    open val channels: MutableMap<String, R> = hashMapOf(),
     beforeProcesses: MutableList<ProcessInfo<T>> = arrayListOf(),
     afterProcesses: MutableList<ProcessInfo<T>> = arrayListOf(),
     catchProcesses: MutableList<ProcessInfo<T>> = arrayListOf()
