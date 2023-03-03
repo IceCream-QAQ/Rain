4f31831ac79648454334861b293ac4b17ee9cbeb
IceCream <www@withdata.net>
2023-03-04 02:54:18 +0800

    调整 DssRouter 结构，分离 静态 Action 与 动态 Action。

--- src/main/kotlin/com/IceCreamQAQ/Yu/controller/dss/router/DssRouter.kt
+++ src/main/kotlin/com/IceCreamQAQ/Yu/controller/dss/router/DssRouter.kt
@@ -8,16 +8,19 @@ open class DssRouter<CTX : PathActionContext>(
     val level: Int
 ) : Router {
 
-    val actions = ArrayList<ActionInvoker<PathActionContext>>()
-
     val staticSubrouter = HashMap<String, DssRouter<CTX>>()
     val dynamicSubrouter = ArrayList<DynamicRouter<CTX>>()
+
+    val staticActions = HashMap<String, MutableList<ActionInvoker<PathActionContext>>>()
+    val dynamicActions = ArrayList<Pair<RouterMatcher<CTX>, MutableList<ActionInvoker<PathActionContext>>>>()
 
     operator fun invoke(context: CTX): Boolean {
         val path = context.path[level]
         if (staticSubrouter[path]?.invoke(context) == true) return true
         if (dynamicSubrouter.any { it.matcher(path, context) && it.router(context) }) return true
-        if (actions.any { it.invoke(context) }) return true
+
+        if (staticActions[path]?.any { it.invoke(context) } == true) return true
+        if (dynamicActions.any { it.first(path, context) && it.second.any { action -> action(context) } }) return true
         return false
     }
 
