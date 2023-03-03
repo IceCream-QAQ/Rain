9c820eafba2cf50c2dde9859a475eb5334f06190
IceCream <www@withdata.net>
2023-03-04 04:26:29 +0800

    补齐 PathActionContext 缺失的内容

--- src/main/kotlin/com/IceCreamQAQ/Yu/controller/dss/PathActionContext.kt
+++ src/main/kotlin/com/IceCreamQAQ/Yu/controller/dss/PathActionContext.kt
@@ -5,6 +5,8 @@ import com.IceCreamQAQ.Yu.controller.ActionContext
 open class PathActionContext(val path: Array<String>) : ActionContext {
 
     val saves = HashMap<String, Any>()
+    override var runtimeError: Throwable? = null
+    override var result: Any? = null
 
     override fun get(name: String) = saves[name]
 
