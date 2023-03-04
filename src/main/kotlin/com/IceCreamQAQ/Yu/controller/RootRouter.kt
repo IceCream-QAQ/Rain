1067423a4b0e47915f406d876eeba31eade1cf1a
IceCream <www@withdata.net>
2023-03-05 03:48:25 +0800

    为 RootRouter 添加 ActionInfo 传递的泛型

--- src/main/kotlin/com/IceCreamQAQ/Yu/controller/RootRouter.kt
+++ src/main/kotlin/com/IceCreamQAQ/Yu/controller/RootRouter.kt
@@ -1,6 +1,6 @@
 package com.IceCreamQAQ.Yu.controller
 
-open class RootRouter<ROT : Router>(
+open class RootRouter<CTX:ActionContext,ROT : Router>(
     val router: ROT,
-    val actions: List<ActionInfo>
+    val actions: List<ActionInfo<CTX>>
 )
