e074be41dcc8cf3f4a77f877365b34e1644cb76a
IceCream <www@withdata.net>
2023-03-12 23:51:34 +0800

    修正 nameWithParamsFullClass 逻辑

--- src/main/kotlin/com/IceCreamQAQ/Yu/fun.kt
+++ src/main/kotlin/com/IceCreamQAQ/Yu/fun.kt
@@ -54,7 +54,7 @@ fun String.toLowerCaseFirstOne(): String {
 
 inline fun <reified T : Annotation> Method.annotation(body: T.() -> Unit) = getAnnotation(T::class.java)?.apply(body)
 
-val Method.fullName: String
+val Executable.fullName: String
     get() {
         val nameBuilder = StringBuilder(this.declaringClass.name).append(".").append(this.name).append("(")
         val max = parameterCount - 1
@@ -81,7 +81,8 @@ val Executable.nameWithParams: String
         .toString()
 
 val Executable.nameWithParamsFullClass: String
-    inline get() = StringBuilder(name)
+    inline get() = StringBuilder(declaringClass.name)
+        .append(".")
         .append("(")
         .apply {
             parameterTypes.let { parameterTypes ->
