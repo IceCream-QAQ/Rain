db4063cd69b0434d2cf743de80c592298e268704
icecream <www@withdata.net>
2024-02-18 00:26:06 +0800

    添加一个基础的 Maven Publish 任务

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,10 @@ val dependencyFastJSON2 by extra("2.0.23")
 val dependencySlf4j by extra("2.0.9")
 
 group = "com.IceCreamQAQ.Rain"
-version = "0.5.0"
+version = "1.0.0-DEV1"
 
 subprojects {
+
     apply {
         plugin("java")
         plugin("java-library")
@@ -29,5 +30,53 @@ subprojects {
         api(kotlin("stdlib"))
         api(kotlin("reflect"))
         api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    }
+
+    configure<PublishingExtension> {
+        publications {
+            create<MavenPublication>(name) {
+                groupId = "com.IceCreamQAQ.Rain"
+                artifactId = name
+                version = "1.0.0-DEV1"
+
+                pom {
+                    name.set("Rain Java Dev Framework")
+                    description.set("Rain Java Dev Framework")
+                    url.set("https://github.com/IceCream-Open/Rain")
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("IceCream")
+                            name.set("IceCream")
+                            email.set("www@withdata.net")
+                        }
+                    }
+                    scm {
+                        connection.set("")
+                    }
+                }
+                from(components["java"])
+            }
+
+            repositories {
+                mavenLocal()
+                maven {
+                    val snapshotsRepoUrl = "https://maven.icecreamqaq.com/repository/maven-snapshots/"
+                    val releasesRepoUrl = "https://maven.icecreamqaq.com/repository/maven-releases/"
+                    url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+
+
+                    credentials {
+                        username = ""
+                        password = ""
+                    }
+                }
+            }
+        }
     }
 }
