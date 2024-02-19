4aa0ae9cf7ec344e0d2595cfe1016fae0e0432aa
icecream <www@withdata.net>
2024-02-19 22:52:30 +0800

    提供 IRainClassloader

--- module/application/src/main/kotlin/rain/application/Application.kt
+++ module/application/src/main/kotlin/rain/application/Application.kt
@@ -6,8 +6,12 @@ import rain.api.di.DiContext.Companion.getBean
 import rain.application.events.AppStatusEvent
 import rain.application.loader.AppLoader
 import rain.application.loader.ApplicationServiceLoader
+import rain.classloader.AppClassloader
+import rain.classloader.IRainClassLoader
 import rain.di.impl.ContextImpl
 import rain.di.config.impl.ConfigImpl
+import rain.di.impl.LocalInstanceClassContext
+import rain.di.impl.NoInstanceClassContext
 import rain.function.exists
 import java.util.*
 
@@ -25,6 +29,13 @@ class Application {
 
         val configManager = ConfigImpl(appClassloader, runMode, launchPackage).init()
         val context = ContextImpl(appClassloader, configManager).init()
+
+        if (appClassloader is AppClassloader){
+            context.contextMap[IRainClassLoader::class.java] = NoInstanceClassContext(context, IRainClassLoader::class.java)
+                .apply {
+                    putBinds("", LocalInstanceClassContext(appClassloader))
+                }
+        }
 
         context.getBean<AppLoader> { error("Application 初始化过程中未能正确获取 Loader！") }.load()
         asLoader = context.getBean()!!
