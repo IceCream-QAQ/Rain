0cad36649f73bebe1e3ab65eb79368c47a2dedd9
icecream <www@withdata.net>
2024-08-29 00:01:16 +0800

    修改 Kotlin 编译目标

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,11 +25,16 @@ subprojects {
         plugin("java")
         plugin("java-library")
         plugin("maven-publish")
+        plugin("org.jetbrains.kotlin.jvm")
     }
 
     java {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlin {
+        jvmToolchain(8)
     }
 
     dependencies {
