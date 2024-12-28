f81d4f4777d6bf2dd30c960914e4d54a06ef61f1
icecream <www@withdata.net>
2024-12-29 03:38:37 +0800

    添加一些 asm 封装函数

--- tools/function/src/main/kotlin/rain/function/asm.kt
+++ tools/function/src/main/kotlin/rain/function/asm.kt
@@ -25,6 +25,15 @@ fun getLoad(type: String): Int =
         else -> ALOAD
     }
 
+fun getStore(type: String): Int =
+    when (type[0]) {
+        'B', 'S', 'I', 'Z', 'C' -> ISTORE
+        'J' -> LSTORE
+        'F' -> FSTORE
+        'D' -> DSTORE
+        else -> ASTORE
+    }
+
 fun getReturn(type: String): Int =
     when (type[0]) {
         'B', 'S', 'I', 'Z', 'C' -> IRETURN
