35276ca38807e6e136e0b033839be3b01f95e7b5
icecream <www@withdata.net>
2025-07-05 03:27:11 +0800

    调整 Gradle 构建环境

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     kotlin("jvm") version "2.0.10"
     java
@@ -34,7 +36,9 @@ subprojects {
     }
 
     kotlin {
-        jvmToolchain(8)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
+        }
     }
 
     dependencies {
