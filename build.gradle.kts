02a1fc94a59c25aa63cead93c26a4f930b4ec75a
icecream <www@withdata.net>
2026-01-15 21:24:31 +0800

    升级 Kotlin 从 2.0.10 到 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    kotlin("jvm") version "2.0.10"
+    kotlin("jvm") version "2.3.0"
     java
     `java-library`
     `maven-publish`
